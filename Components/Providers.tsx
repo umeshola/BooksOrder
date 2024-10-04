3a5cba87b22fae181f53d501f199a2d6a61d7cdb
UmeshOla <umeshola07@gmail.com>
2024-10-04 13:46:33 +0530

    matching the fild

--- Components/Providers.tsx
+++ Components/Providers.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { ApolloClient, InMemoryCache, ApolloProvider, gql } from "@apollo/client";
 export const Providers = ({ children }: { children: React.ReactNode }) => {
     const client = new ApolloClient({
-        uri: "https://books-order-ukp1.vercel.app/api/graphql",
+        uri: "https://books-order.vercel.app/api/graphql",
         cache: new InMemoryCache(),
     })
     return <ApolloProvider client={client}>{children}</ApolloProvider>
